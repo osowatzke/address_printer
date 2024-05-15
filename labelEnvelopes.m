86de23270c031ac860fbf3be6959a5928e43460f
Owen <osowatzke@gmail.com>
2024-05-14 22:26:37 -0700

    Saving work

--- labelEnvelopes.m
+++ labelEnvelopes.m
@@ -1,5 +1,5 @@
 word = actxserver('Word.Application');
-word.Visible = 1;
+word.Visible = 0;
 document = word.Documents.Add;
 page = document.ActiveWindow.ActivePane.Pages.Item(1);
 width = page.Width;
@@ -35,52 +35,3 @@ addrPdfFileName = fullfile(currFilePath,'Addresses.pdf');
 document.SaveAs2(addrDocxFileName);
 document.SaveAs2(addrPdfFileName,17);
 word.Quit();
-
-% function pageBreak(selection)
-%     selection.InsertNewPage;
-%     selection.MoveUp(4,1);
-%     selection.MoveUp(5,1);
-%     selection.MoveUntil(char(13));
-%     selection.Delete;
-%     selection.MoveDown(4,1);
-% end
-% 
-% function printAddress(selection, addr)
-%     fontSel = cellfun(@(x) any(regexp(x,'[0-9]')), addr.name);
-%     fontSel = any(fontSel);
-%     if fontSel
-%         selection.Font.Name='Aparajita';
-%         selection.Font.Size=20;
-%     else
-%         selection.Font.Name='Cormorant Garamond';
-%         selection.Font.Size=16;
-%     end
-%     for i = 1:length(addr.name)
-%         if i == length(addr.name)
-%             selection.Paragraphs.LineSpacing = 18;
-%         else
-%             selection.Paragraphs.LineSpacing = 12;
-%         end
-%         selection.Paragraphs.SpaceBefore = 0;
-%         selection.Paragraphs.SpaceAfter = 0;
-%         selection.TypeText(upper(addr.name{i}));
-%         if i ~= length(addr.name) || ~isempty(addr.addr)
-%             selection.TypeParagraph;
-%         end
-%     end
-%     selection.Font.Name='GeosansLight';
-%     selection.Font.Size=12;
-%     for i = 1:length(addr.addr)
-%         if i == length(addr.addr)
-%             selection.Paragraphs.LineSpacing = 18;
-%         else
-%             selection.Paragraphs.LineSpacing = 12;
-%         end
-%         selection.Paragraphs.SpaceBefore = 0;
-%         selection.Paragraphs.SpaceAfter = 0;
-%         selection.TypeText(upper(addr.addr{i}));
-%         if i ~= length(addr.addr)
-%             selection.TypeParagraph;
-%         end
-%     end
-% end
